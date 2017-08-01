2ee1e1e6d983c65966bec15de627b82d33f66040
kofu <ko.fu.dev@gmail.com>
2017-08-02 01:16:32 +0900

    implement convert hieroglyph to japanese(probably, contains bug. haha)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-function convert() {
-    const resultIsSuccess = japaneseToHieroglyph(document.getElementById("source").value,
-        document.getElementById("result"));
+function convertJapanese() {
+    const resultIsSuccess = japaneseToHieroglyph(document.getElementById("japanese").value,
+        document.getElementById("hieroglyph"));
     const errorView = document.getElementById("error");
     if (!resultIsSuccess) {
         errorView.style.display = "inherit";
     } else {
         errorView.style.display = "none";
     }
+}
+
+function convertHieroglyph() {
+    const resultIsSuccess = hieroglyphToJapanese(document.getElementById("hieroglyph").value,
+        document.getElementById("japanese"));
+    const errorView = document.getElementById("error");
+    if (!resultIsSuccess) {
+        errorView.style.display = "inherit";
+    } else {
+        errorView.style.display = "none";
+    }
+
+    document.getElementById("beta_warning").style.display = "inherit";
 }
