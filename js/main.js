c7b490e314745cdca7d6d121f0593e7026f916b2
kofu <ko.fu.dev@gmail.com>
2017-08-03 02:13:42 +0900

    improve appearance(materialize)

--- js/main.js
+++ js/main.js
@@ -21,3 +21,24 @@ function convertHieroglyph() {
 
     document.getElementById("beta_warning").style.display = "inherit";
 }
+
+let convertDirection = 0;
+
+const convertButton = document.getElementById("convert");
+
+function switchConvertDirection() {
+    convertDirection = convertDirection === 0 ? 1 : 0;
+    Velocity(convertButton, {rotateZ: convertDirection === 0 ? 0 : 180}, {duration: 500});
+}
+
+function convertWithSelectedDirection() {
+    if (convertDirection === 0) {
+        convertJapanese();
+    } else {
+        convertHieroglyph();
+    }
+}
+
+function expandLicenseSection() {
+    document.getElementById("license").style.display = "inherit";
+}
